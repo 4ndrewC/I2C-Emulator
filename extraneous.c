3741f228998f68a820f45a141b0a97cba950efc4
4ndrewC <84311342+4ndrewC@users.noreply.github.com>
2024-03-28 15:47:13 +0800

    Update extraneous.c

--- extraneous.c
+++ extraneous.c
@@ -13,7 +13,8 @@ byte convert(char *str) {
     return (byte)result;
 }
 
-void rconvert(int num, char *binary_str) {
+char* rconvert(int num) {
+  char* binary_str;
   int i = 0;
   if (num < 0) {
     num = (num << 1) + 1;
@@ -29,4 +30,5 @@ void rconvert(int num, char *binary_str) {
     binary_str[j] = binary_str[i - j - 1];
     binary_str[i - j - 1] = temp;
   }
+  return binary_str;
 }
